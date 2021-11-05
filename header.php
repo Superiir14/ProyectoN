43a91fb937fcd2aab2a68e7b8f6b9e9ee88f7de1
Superiir14 <85659594+Superiir14@users.noreply.github.com>
2021-11-05 13:31:00 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>COPPEL</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="imagenes/coppel.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="Cliente.php">Cliente</a></li>
+					<li><a href="Articulo.php">Articulo</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
